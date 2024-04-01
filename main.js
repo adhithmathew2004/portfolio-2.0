65c597116156b293127be1b02c7c63acf692d980
adhithmathew <adhithmathew0@example.com>
2024-04-01 20:06:27 +0530

    add CNAME

--- main.js
+++ main.js
@@ -10,22 +10,7 @@ $(document).ready(function(){
       $('nav').removeClass('nav-toggle');
     });
   
-    $('.count').each(function() {
-      var $this = $(this),
-          countTo = $this.attr('data-count');
-      $({ countNum: $this.text()}).animate({
-        countNum: countTo
-      },
-      {
-        duration: 5000,
-        step: function() {
-          $this.text(Math.floor(this.countNum));
-        },
-        complete: function() {
-          $this.text(this.countNum + '+');
-        }
-      });
-    });
+
   
     $('.project').magnificPopup({
       delegate:'a',
