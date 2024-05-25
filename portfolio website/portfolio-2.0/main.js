79e5902eddb2dab4fbce236c19d4417ba3411b5d
adhithmathew2004 <160561100+adhithmathew2004@users.noreply.github.com>
2024-05-26 01:51:46 +0530

    Add files via upload

---
+++ portfolio website/portfolio-2.0/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+    $('.fa-bars').click(function(){
+      $(this).toggleClass('fa-times');
+      $('nav').toggleClass('nav-toggle');
+    });
+  
+    $(window).on('scroll load',function(){
+      $('.fa-bars').removeClass('fa-times');
+      $('nav').removeClass('nav-toggle');
+    });
+  
+
+  
+    $('.project').magnificPopup({
+      delegate:'a',
+      type:'image',
+      gallery:{
+        enabled:true
+      }
+    });
+  
+  });
+
+
+  
